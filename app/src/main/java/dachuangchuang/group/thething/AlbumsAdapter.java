71fe9dda0f30819a8def90c255577609a355ca72
beloveby <645942292@qq.com>
2016-02-12 21:08:28 +0800

    Some job with tabs

---
+++ app/src/main/java/dachuangchuang/group/thething/AlbumsAdapter.java
@@ -0,0 +1,30 @@
+package dachuangchuang.group.thething;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by 边园 on 2016/2/12.
+ */
+public class AlbumsAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
