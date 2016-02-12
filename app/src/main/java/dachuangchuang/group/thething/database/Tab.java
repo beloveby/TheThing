35176da04f566850e752001864984ceeb29ee0ff
beloveby <645942292@qq.com>
2016-02-12 21:11:46 +0800

    Some job with tabs

---
+++ app/src/main/java/dachuangchuang/group/thething/database/Tab.java
@@ -0,0 +1,26 @@
+package dachuangchuang.group.thething.database;
+
+/**
+ * Created by 边园 on 2016/2/10.
+ */
+public class Tab {
+    private int tabId;
+    private String tabName;
+
+
+    public int getTabId() {
+        return tabId;
+    }
+
+    public void setTabId(int tabId) {
+        this.tabId = tabId;
+    }
+
+    public String getTabName() {
+        return tabName;
+    }
+
+    public void setTabName(String tabName) {
+        this.tabName = tabName;
+    }
+}
